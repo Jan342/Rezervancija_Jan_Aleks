82a5bbafff10ecfc340536220b78a4d60029e521
creepy-boop <alekshj2004@gmail.com>
2023-03-14 09:35:24 +0100

    test

---
+++ src/LoginPage.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class LoginPage {
+    private JLabel Login;
+    private JTextField password_text;
+    private JTextField username_text;
+    private JLabel username;
+    private JLabel Passowrd;
+}
