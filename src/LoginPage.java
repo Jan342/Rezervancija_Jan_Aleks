ccf82470d139dac8c391d8719ce09a4dc2c00f9c
Jan21 <jan.jereb55@gmail.com>
2023-05-07 14:12:17 +0200

    LOGIN dodal change password

--- src/LoginPage.java
+++ src/LoginPage.java
@@ -7,7 +7,27 @@ public class LoginPage extends JFrame implements ActionListener {
     private JLabel loginLabel, usernameLabel, passwordLabel, messageLabel;
     private JTextField usernameText, passwordText;
     private JButton submitButton;
+    private JButton ChangePassword;
+    Connection con;
+    PreparedStatement pst;
+    CallableStatement stmt = null;
 
+    public void connect() {
+        try {
+            Class.forName("org.postgresql.Driver");
+            con = DriverManager.getConnection("jdbc:postgresql://ep-lingering-grass-902680.eu-central-1.aws.neon.tech:5432/neondb",
+                    "alekshj2004", "ZMmQk5fyAKx4");
+            System.out.println("works");
+        }
+        catch (ClassNotFoundException ex)
+        {
+            ex.printStackTrace();
+        }
+        catch (SQLException ex)
+        {
+            ex.printStackTrace();
+        }
+    }
     public LoginPage() {
         loginLabel = new JLabel("Login");
         usernameLabel = new JLabel("Username:");
@@ -16,7 +36,10 @@ public class LoginPage extends JFrame implements ActionListener {
         usernameText = new JTextField(20);
         passwordText = new JPasswordField(20);
         submitButton = new JButton("Submit");
+        ChangePassword = new JButton("Change Password");
         submitButton.addActionListener(this);
+        ChangePassword.addActionListener(this);
+
 
 
         JPanel panel = new JPanel(new GridLayout(4, 2));
@@ -27,6 +50,8 @@ public class LoginPage extends JFrame implements ActionListener {
         panel.add(passwordLabel);
         panel.add(passwordText);
         panel.add(submitButton);
+        panel.add(ChangePassword);
+
 
         panel.add(messageLabel);
 
@@ -35,10 +60,160 @@ public class LoginPage extends JFrame implements ActionListener {
         setSize(400, 200);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
+
+    }
+    private void showChangePasswordDialog() {
+        JDialog changePasswordDialog = new JDialog(this, "Change Password", true);
+        ChangePasswordPanel changePasswordPanel = new ChangePasswordPanel();
+        changePasswordDialog.add(changePasswordPanel);
+        changePasswordDialog.pack();
+        changePasswordDialog.setLocationRelativeTo(this);
+        changePasswordDialog.setVisible(true);
     }
 
+
+
+    public class ChangePasswordPanel extends JPanel {
+        private JLabel usernameLabel;
+        private JLabel currentPasswordLabel;
+        private JLabel newPasswordLabel;
+        private JTextField usernameText;
+        private JPasswordField currentPasswordText;
+        private JPasswordField newPasswordText;
+        private JButton SubmitButton;
+        private JLabel messageLabel;
+
+        public ChangePasswordPanel() {
+            usernameLabel = new JLabel("username");
+            currentPasswordLabel = new JLabel("Current Password:");
+            newPasswordLabel = new JLabel("New Password:");
+            usernameText = new JTextField(20);
+            currentPasswordText = new JPasswordField(20);
+            newPasswordText = new JPasswordField(20);
+            SubmitButton = new JButton("Submit");
+            messageLabel = new JLabel("");
+
+            SubmitButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    changePassword();
+                }
+            });
+
+            setLayout(new GridLayout(5, 2));
+            add(usernameLabel);
+            add(usernameText);
+            add(currentPasswordLabel);
+            add(currentPasswordText);
+            add(newPasswordLabel);
+            add(newPasswordText);
+            add(SubmitButton);
+            add(messageLabel);
+
+        }
+
+        private void changePassword() {
+            String username = usernameText.getText();
+            String currentPassword = new String(currentPasswordText.getPassword());
+            String newPassword = new String(newPasswordText.getPassword());
+
+            if (ValidateLogin(username, currentPassword)) {
+                if (updatePasswordInDatabase(username, newPassword)) {
+                    messageLabel.setText("Password changed successfully!");
+                } else {
+                    messageLabel.setText("Error changing password.");
+                }
+            } else {
+                messageLabel.setText("Invalid username or current password.");
+            }
+        }
+
+        private boolean ValidateLogin(String username, String password) {
+
+            ResultSet rs = null;
+            boolean isValid = false;
+
+            try {
+                connect();
+                // Prepare a call to the validate_login function
+                String sql = "{ ? = call validate_login(?, ?) }";
+                stmt = con.prepareCall(sql);
+                stmt.registerOutParameter(1, Types.BOOLEAN);
+                stmt.setString(2, username);
+                stmt.setString(3, password);
+
+                // Execute the call and get the result
+                stmt.execute();
+                isValid = stmt.getBoolean(1);
+            } catch (SQLException se) {
+                // Handle errors for JDBC
+                se.printStackTrace();
+            } catch (Exception ex) {
+                // Handle errors for Class.forName
+                ex.printStackTrace();
+            } finally {
+                // Close resources
+                try {
+                    if (rs != null) rs.close();
+                } catch (SQLException se) {
+                    se.printStackTrace();
+                }
+                try {
+                    if (stmt != null) stmt.close();
+                } catch (SQLException se) {
+                    se.printStackTrace();
+                }
+                try {
+                    if (con != null) con.close();
+                } catch (SQLException se) {
+                    se.printStackTrace();
+                }
+            }
+
+            return isValid;
+        }
+
+        private boolean updatePasswordInDatabase(String username, String newPassword) {
+
+            PreparedStatement stmt = null;
+            int rowsAffected = 0;
+
+            try {
+                connect();
+
+                // Create an SQL update statement
+                String sql = "UPDATE uporabniki SET password = ? WHERE username = ?";
+                stmt = con.prepareStatement(sql);
+                stmt.setString(1, newPassword);
+                stmt.setString(2, username);
+
+                // Execute the update statement and get the number of affected rows
+                rowsAffected = stmt.executeUpdate();
+            } catch (SQLException se) {
+                // Handle errors for JDBC
+                se.printStackTrace();
+            } catch (Exception ex) {
+                // Handle errors for Class.forName
+                ex.printStackTrace();
+            } finally {
+                // Close resources
+                try {
+                    if (stmt != null) stmt.close();
+                } catch (SQLException se) {
+                    se.printStackTrace();
+                }
+                try {
+                    if (con != null) con.close();
+                } catch (SQLException se) {
+                    se.printStackTrace();
+                }
+            }
+            return rowsAffected > 0;
+
+        }
+    }
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == submitButton) {
+       if (e.getSource() == submitButton) {
             String username = usernameText.getText();
             String password = passwordText.getText();
             if (username.equals("admin") && password.equals("admin123")) {
@@ -46,12 +221,14 @@ public class LoginPage extends JFrame implements ActionListener {
             } else {
                 messageLabel.setText("Invalid username or password");
             }
+        } else if (e.getSource() == ChangePassword) {
+            showChangePasswordDialog();
         } else {
             usernameText.setText("");
             passwordText.setText("");
         }
     }
-    public boolean validateLogin(String username, String password) {
+/*   public boolean validateLogin(String username, String password) {
         Connection conn = null;
         CallableStatement stmt = null;
         ResultSet rs = null;
@@ -103,7 +280,7 @@ public class LoginPage extends JFrame implements ActionListener {
         }
 
         return isValid;
-    }
+    }*/
     public static void main(String[] args) {
         new LoginPage();
     }
