f9fdb10be6f87cbed4ecbdbc92fbca8982ce76a9
Jan21 <jan.jereb55@gmail.com>
2023-05-07 12:19:50 +0200

    confirm password dodan na registracijo

--- src/RegPage.java
+++ src/RegPage.java
@@ -1,5 +1,9 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class RegPage {
@@ -12,10 +16,14 @@ public class RegPage {
     private JPanel panel1;
     private JFrame frame;
 
+    Connection con;
+    PreparedStatement pst;
+
     public void connect() {
         try {
             Class.forName("org.postgresql.Driver");
-            con = DriverManager.getConnection("jdbc:postgresql://snuffleupagus.db.elephantsql.com:5432/hbfssypp", "hbfssypp", "Oq5dieXemgwvy-Qhf8MB3dFl2K5Q0-CR");
+            con = DriverManager.getConnection("jdbc:postgresql://ep-lingering-grass-902680.eu-central-1.aws.neon.tech:5432/neondb",
+                    "alekshj2004", "ZMmQk5fyAKx4");
             System.out.println("works");
         }
         catch (ClassNotFoundException ex)
@@ -37,5 +45,46 @@ public class RegPage {
         frame.setSize(600,500);
         frame.setVisible(true);
 
-        connect();    }
+        connect();
+        regButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+                String name,email,pass,confirmPass;
+
+
+                email = emailText.getText();
+                name= userText.getText();
+                pass = passText.getText();
+                confirmPass = confpassText.getText();
+
+                if (!pass.equals(confirmPass)) {
+                    JOptionPane.showMessageDialog(null, "Passwords do not match!");
+                    return;
+                }
+
+
+
+                try{
+                    pst = con.prepareStatement("insert into uporabniki (username, password, email) values(?,?,?)");
+                    pst.setString(1,name);
+                    pst.setString(2, pass);
+                    pst.setString(3,email);
+                    pst.executeUpdate();
+                    JOptionPane.showMessageDialog(null,"Registered!");
+
+                    emailText.setText("");
+                    userText.setText("");
+                    passText.setText("");
+                    emailText.requestFocus();
+
+
+                }catch (SQLException e1){
+                    e1.printStackTrace();
+
+                }
+
+            }
+        });
+    }
 }
